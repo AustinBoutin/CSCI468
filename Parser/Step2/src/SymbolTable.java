82cb2e1b6a3a8ac69947d656547c8dd0a25a0fd6
Jake Petek <Jake Petek>
2017-03-29 17:55:45 -0600

    Made methods for SymbolTable

--- Parser/Step2/src/SymbolTable.java
+++ Parser/Step2/src/SymbolTable.java
@@ -1,4 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class SymbolTable {
-
+	private Scope root;
+	private Scope cur;
+	
+	public SymbolTable()
+	{
+		root = new Scope();
+		root.name = "GLOBAL";
+		root.children = new ArrayList<Scope>();
+		cur = root;
+	}
+	
+	public void addScope(String name){
+		Scope s = new Scope();
+		s.name = name;
+		s.parent = cur;
+		s.children = new ArrayList<Scope>();
+		cur.children.add(s);
+		cur = s;
+	}
+	
+	public void addSymbol(String type, String name, String value){
+		Symbol s = new Symbol();
+		s.name = name;
+		s.type = type;
+		s.value = value;
+		cur.symbols.add(s);
+	}
+	
+	public void moveCurToParent(){
+		cur = cur.parent;
+	}
+	
+	public static class Scope {
+		private String name;
+		private Scope parent;
+		private List<Scope> children;
+		private List<Symbol> symbols = new ArrayList<Symbol>();
+	}
+	
+	public static class Symbol{
+		private String name;
+		private String type;
+		private String value;
+	}
 }
